c9d3b7fe8ed2f4ea1595013fcb78e27832f283f3
Daniel Soares <danielsoares@MacBook-Air-de-Daniel.local>
2026-03-03 00:16:37 -0300

    feat: setup gtm tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Sora, Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ClientLayoutWrapper from "@/components/ClientLayoutWrapper";
 
@@ -33,9 +34,31 @@ export default function RootLayout({
         <script src="https://unpkg.com/@phosphor-icons/web" async></script>
       </head>
       <body className="antialiased font-sans bg-black text-white min-h-screen flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2J9Q6RC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ClientLayoutWrapper>
           {children}
         </ClientLayoutWrapper>
+
+        {/* Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2J9Q6RC');
+          `}
+        </Script>
+
+
       </body>
     </html>
   );
